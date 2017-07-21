044851594a35b7ba17ed2939e73863b39247fd74
chengyurou <18829292695@163.com>
2017-07-21 09:43:58 +0800

    finish 14_AnonymousMethod.cs

--- src/BanKai.Basic/14_AnonymousMethod.cs
+++ src/BanKai.Basic/14_AnonymousMethod.cs
@@ -10,7 +10,7 @@ namespace BanKai.Basic
         [Fact]
         public void should_write_anonymous_inplace()
         {
-            Func<int, int> doubleTransform = delegate(int x)
+            Func<int, int> doubleTransform = delegate(int x)  //匿名函数 delegate(patameters){implementtationCode}
             {
                 return x * 2;
             };
@@ -18,7 +18,7 @@ namespace BanKai.Basic
             int transformResult = doubleTransform(2);
 
             // change variable value to fix test.
-            const int expectedResult = default(int);
+            const int expectedResult = 4;
 
             Assert.Equal(expectedResult, transformResult);
         }
@@ -26,12 +26,15 @@ namespace BanKai.Basic
         [Fact]
         public void should_write_anonymous_method_in_a_more_simple_way()
         {
-            Func<int, int> doubleTransform = x => x * 2;
+            Func<int, int> doubleTransform = x => x * 2;  
+            //lambda表达式，func是委托，编译器知道将方法赋值给委托，故delegate可省略
+            //从委托声明中得知了委托参数类型为int，故int可省略
+            //只有一个委托参数，（）省略，块里面只有一条语句{}省略
 
             int transformResult = doubleTransform(2);
 
             // change variable value to fix test.
-            const int expectedResult = default(int);
+            const int expectedResult = 4;
 
             Assert.Equal(expectedResult, transformResult);
         }
