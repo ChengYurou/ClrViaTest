2a5ee69bad1ada3301b62922cd6b91cf5792f248
chengyurou <18829292695@163.com>
2017-07-26 11:37:04 +0800

    finish NumberSequence.cs

--- src/BanKai.Progress.LinqRelated/NumberSequence.cs
+++ src/BanKai.Progress.LinqRelated/NumberSequence.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BanKai.Progress.LinqRelated
@@ -8,7 +9,7 @@ namespace BanKai.Progress.LinqRelated
         [Fact]
         public void should_get_number_sequence_incrementally()
         {
-            int[] expectedResult = {1, 2, 3, 4, 5};
+            int[] expectedResult = { 1, 2, 3, 4, 5 };
 
             Assert.Equal(expectedResult, GetNumberSequence(1, 5));
         }
@@ -16,7 +17,7 @@ namespace BanKai.Progress.LinqRelated
         [Fact]
         public void should_get_number_sequence_decrementally()
         {
-            int[] expectedResult = {5, 4, 3, 2, 1};
+            int[] expectedResult = { 5, 4, 3, 2, 1 };
 
             Assert.Equal(expectedResult, GetNumberSequence(5, 1));
         }
@@ -24,15 +25,40 @@ namespace BanKai.Progress.LinqRelated
         [Fact]
         public void should_get_number()
         {
-            int[] expectedResult = {5};
+            int[] expectedResult = { 5 };
 
             Assert.Equal(expectedResult, GetNumberSequence(5, 5));
         }
 
+        //static IEnumerable<int> GetNumberSequence(int start, int end)
+        //{
+        //    var numberList = end >= start
+        //        ? Enumerable.Range(start, end - start + 1).ToList()
+        //        : Enumerable.Range(end, start - end + 1).Reverse().ToList();
+
+        //    return numberList;
+        //}
+
         static IEnumerable<int> GetNumberSequence(int start, int end)
         {
-            // TODO: write your code here.
-            throw new System.NotImplementedException();
+            var numberList = new List<int> { };
+
+            if(start <= end)
+            {
+                for (int number = start; number <= end; number++)
+                {
+                    numberList.Add(number);
+                }
+            }
+            else
+            {
+                for(int number = start; number >= end; number--)
+                {
+                    numberList.Add(number);
+                }
+            }
+
+            return numberList;
         }
     }
 }
