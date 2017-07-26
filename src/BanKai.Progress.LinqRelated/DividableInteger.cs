75df44b143662ff3460506cf15cbb1ee58f4148a
chengyurou <18829292695@163.com>
2017-07-26 10:36:44 +0800

    finish DividableInteger.cs

--- src/BanKai.Progress.LinqRelated/DividableInteger.cs
+++ src/BanKai.Progress.LinqRelated/DividableInteger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xunit;
+using System.Linq;
 
 namespace BanKai.Progress.LinqRelated
 {
@@ -11,7 +12,7 @@ namespace BanKai.Progress.LinqRelated
         {
             int[] collectionA = { 4, 7, 9, 25, 16, 21, 64, 32, 35, 49 };
             int[] collectionB = { 2, 3, 5 };
-            int[] expectedResults = {4, 9, 16, 21, 25, 32, 35, 64};
+            int[] expectedResults = { 4, 9, 16, 21, 25, 32, 35, 64 };
 
             Assert.Equal(
                 expectedResults,
@@ -22,7 +23,36 @@ namespace BanKai.Progress.LinqRelated
             IEnumerable<int> source,
             IEnumerable<int> dividers)
         {
-            throw new NotImplementedException();
+            //var result = new List<int> { };
+
+            var result = from itemA in source
+                         from itemB in dividers
+                         where (itemA / itemB != 0 && itemA % itemB == 0)
+                         orderby itemA
+                         select itemA;
+            return result;
         }
+
+        //IEnumerable<int> ChooseDividableNumberFromCollection(
+        //    IEnumerable<int> source,
+        //    IEnumerable<int> dividers)
+        //{
+        //    var result = new List<int> { };
+        //    foreach(int itemA in source)
+        //    {
+        //        //bool temp = false;
+        //        foreach(int itemB in dividers)
+        //        {
+        //            if (itemA / itemB != 0 && itemA%itemB == 0)
+        //            {
+        //                result.Add(itemA);
+        //                break;
+        //            }
+        //        }
+        //    }
+
+        //    return result.OrderBy(item => item);
+        //}
+
     }
 }
