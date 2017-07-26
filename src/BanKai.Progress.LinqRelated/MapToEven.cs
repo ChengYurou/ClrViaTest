c709a49d7e545e6e454d6a0fa97024e2a4ea769a
chengyurou <18829292695@163.com>
2017-07-26 11:58:56 +0800

    finish MapToEven.cs

--- src/BanKai.Progress.LinqRelated/MapToEven.cs
+++ src/BanKai.Progress.LinqRelated/MapToEven.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BanKai.Progress.LinqRelated
@@ -10,13 +11,15 @@ namespace BanKai.Progress.LinqRelated
         {
             int[] collection = { 1, 2, 3, 4, 5 };
             int[] expected = { 2, 4, 6, 8, 10 };
-            
+
             Assert.Equal(expected, MapEven(collection));
         }
 
         static IEnumerable<int> MapEven(IEnumerable<int> collection)
         {
-            throw new System.NotImplementedException();
+            var result = collection.Select(item => item * 2);
+
+            return result;
         }
     }
 }
