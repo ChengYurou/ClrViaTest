4ed7beb9ba9312200b6537fc287164860675fa31
chengyurou <18829292695@163.com>
2017-07-26 11:20:00 +0800

    finish EvenNumberSequence.cs

--- src/BanKai.Progress.LinqRelated/EvenNumberSequence.cs
+++ src/BanKai.Progress.LinqRelated/EvenNumberSequence.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BanKai.Progress.LinqRelated
@@ -8,7 +10,7 @@ namespace BanKai.Progress.LinqRelated
         [Fact]
         public void should_get_even_integer_sequence_increment()
         {
-            int[] expected = {2, 4, 6, 8, 10};
+            int[] expected = { 2, 4, 6, 8, 10 };
             IEnumerable<int> result = GetEvenSequence(1, 10);
 
             Assert.Equal(expected, result);
@@ -17,7 +19,7 @@ namespace BanKai.Progress.LinqRelated
         [Fact]
         public void should_get_even_integer_sequence_decrement()
         {
-            int[] expected = {10, 8, 6, 4, 2};
+            int[] expected = { 10, 8, 6, 4, 2 };
             IEnumerable<int> result = GetEvenSequence(10, 1);
 
             Assert.Equal(expected, result);
@@ -32,6 +34,7 @@ namespace BanKai.Progress.LinqRelated
             Assert.Equal(expected, result);
         }
 
+        [Fact]
         public void should_get_nothing_if_start_and_end_are_same_odd_number()
         {
             int[] expected = { };
@@ -40,10 +43,43 @@ namespace BanKai.Progress.LinqRelated
             Assert.Equal(expected, result);
         }
 
+        //static IEnumerable<int> GetEvenSequence(int start, int end)
+        //{
+        //    var numberList = new List<int> { };
+
+        //    if (end >= start)
+        //    {
+        //        for (int num = start; num <= end; num++)
+        //        {
+        //            numberList.Add(num);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for (int num = start; num >= end; num--)
+        //        {
+        //            numberList.Add(num);
+        //        }
+        //    }
+
+        //    var result = from number in numberList
+        //                 where number % 2 == 0
+        //                 select number;
+
+        //    return result;
+        //}
+
         static IEnumerable<int> GetEvenSequence(int start, int end)
         {
-            // TODO: write your implementation here.
-            throw new System.NotImplementedException();
+            var numberList = end >= start
+                ? Enumerable.Range(start, end - start + 1).ToList()
+                : Enumerable.Range(end, start - end + 1).Reverse().ToList();
+
+            var result = from number in numberList
+                         where number % 2 == 0
+                         select number;
+
+            return result;
         }
     }
 }
